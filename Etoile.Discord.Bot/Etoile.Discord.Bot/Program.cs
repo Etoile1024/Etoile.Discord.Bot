e9c4150aef5fa5bc7065919038fc6c3b71a6977b
Etoile <Etoile@DESKTOP-9LVM81A>
2020-11-08 15:47:48 +0800

    First submission

---
+++ Etoile.Discord.Bot/Etoile.Discord.Bot/Program.cs
@@ -0,0 +1,14 @@
+﻿using Etoile.Discord.Bot.Cores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etoile.Discord.Bot
+{
+    class Program
+    {
+        static void Main(string[] args) => new DiscordBot().RunBotAsync().GetAwaiter().GetResult();
+    }
+}
