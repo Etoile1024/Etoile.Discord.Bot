e5f62d2aa2e1f74284a202195226d01ab1c266ae
Etoile <Etoile@LAPTOP-IDS8ARRV>
2020-11-12 13:33:00 +0800

    Add dice function

--- Etoile.Discord.Bot/Etoile.Discord.Bot/Commands/BasicCommand.cs
+++ Etoile.Discord.Bot/Etoile.Discord.Bot/Commands/BasicCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Commands;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,9 +25,48 @@ namespace Etoile.Discord.Bot.Commands
                 .AddField("?np", "睇下唱緊咩歌")
                 .AddField("?list", "列下有咩歌會唱")
                 .AddField("?skip", "quit左首歌佢囉！")
-                .AddField("?volume 數值", "set佢有幾嘈");
+                .AddField("?volume 數值", "set佢有幾嘈")
+                .AddField("?dice 擲骰子次數d骰子最大值", "擲骰子");
 
             await ReplyAsync("", false, embedBuilder.Build());
+        }
+        [Command("dice")]
+        public async Task DiceCmd(string request)
+        {
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            try
+            {
+                int count = Convert.ToInt32(request.Split('d')[0]);
+                int max = Convert.ToInt32(request.Split('d')[1]);
+                if (count > 500)
+                {
+                    embedBuilder.WithTitle("エラー").WithDescription("最多只可以擲500次骰。").WithColor(Color.Red);
+                    await ReplyAsync("", false, embedBuilder.Build());
+                    return;
+                }
+                if (max > 100)
+                {
+                    embedBuilder.WithTitle("エラー").WithDescription("骰子最大值最多只可以係100。").WithColor(Color.Red);
+                    await ReplyAsync("", false, embedBuilder.Build());
+                    return;
+                }
+                string diceArray = string.Empty;
+                int sum = 0;
+                for (int i = 1; i <= count; i++)
+                {
+                    int result = new Random(Guid.NewGuid().GetHashCode()).Next(1, max + 1);
+                    sum += result;
+                    diceArray = diceArray == string.Empty ? result.ToString() : string.Format("{0}, {1}", diceArray, result);
+                }
+                string message = string.Format("{0}嘅擲骰子過程：`[{1}]` 結果：{2}", Context.User.Mention, diceArray, sum);
+                await ReplyAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error on dice command:\r\n{0}", ex);
+                embedBuilder.WithTitle("エラー").WithDescription("擲骰子失敗！請檢查你所輸入嘅指令係咪啱！").WithColor(Color.Red);
+                await ReplyAsync("", false, embedBuilder.Build());
+            }
         }
     }
 }
